fb447cacf4d539db1023f976d8e7261b4110b055
J0e101 <josephonuonga5@gmail.com>
2025-02-27 10:53:18 +0300

    Hosii

--- HospitalApp/urls.py
+++ HospitalApp/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='home'),
     path('starter-page/', views.starter, name='starter-page'),
-    path('service-details', views.service, name='service-details'),
+    path('service-details/', views.service, name='service-details'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('departments/', views.deps, name='departments'),
+    path('doctors/', views.doctors, name='doctors'),
 
 ]
--- HospitalApp/views.py
+++ HospitalApp/views.py
@@ -9,3 +9,15 @@ def starter(request):
 
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def doctors(request):
+    return render(request, 'doctors.html')
+
+def deps(request):
+    return render(request, 'departments.html')
